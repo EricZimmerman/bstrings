52bcc56f6f893b1979c1455795efacf283fbdfe2
EricZimmerman <saericzimmerman@gnail.com>
2016-02-01 16:45:02 -0700

    - Added --off switch to show offsets of string hits. The type of hit will be shown in parenthesis after the offset (A=ASCII, U=Unicode) - Change from UTF8 to Windows-1252 encoding to find non-Unicode strings

--- bstrings/Program.cs
+++ bstrings/Program.cs
@@ -709,7 +709,7 @@ namespace bstrings
                 {
                     var actualOffset = (currentOffset + match.Index) * 2;
 
-                    hits.Add($"{match.Value.Trim()}{'\0'}0x{actualOffset:X}");
+                    hits.Add($"{match.Value.Trim()}{'\t'}0x{actualOffset:X} (U)");
                 }
                 else
                 {
@@ -720,6 +720,53 @@ namespace bstrings
             return hits; 
         }
 
+        private static int ByteSearch(byte[] searchIn, byte[] searchBytes, int start = 0)
+        {
+            int found = -1;
+            bool matched = false;
+            //only look at this if we have a populated search array and search bytes with a sensible start
+            if (searchIn.Length > 0 && searchBytes.Length > 0 && start <= (searchIn.Length - searchBytes.Length) && searchIn.Length >= searchBytes.Length)
+            {
+                //iterate through the array to be searched
+                for (int i = start; i <= searchIn.Length - searchBytes.Length; i++)
+                {
+                    //if the start bytes match we will start comparing all other bytes
+                    if (searchIn[i] == searchBytes[0])
+                    {
+                        if (searchIn.Length > 1)
+                        {
+                            //multiple bytes to be searched we have to compare byte by byte
+                            matched = true;
+                            for (int y = 1; y <= searchBytes.Length - 1; y++)
+                            {
+                                if (searchIn[i + y] != searchBytes[y])
+                                {
+                                    matched = false;
+                                    break;
+                                }
+                            }
+                            //everything matched up
+                            if (matched)
+                            {
+                                found = i;
+                                break;
+                            }
+
+                        }
+                        else
+                        {
+                            //search byte is only one bit nothing else to do
+                            found = i;
+                            break; //stop the loop
+                        }
+
+                    }
+                }
+
+            }
+            return found;
+        }
+
         private static List<string> GetAsciiHits(byte[] bytes, int minSize, int maxSize, long currentOffset, bool withOffsets)
         {
             var maxString = maxSize == -1 ? "" : maxSize.ToString();
@@ -727,31 +774,21 @@ namespace bstrings
 
             const string ascRange = "[\x20-\x7E]";
             var regUni = new Regex($"{ascRange}{mi2}");
-            var utfString = Encoding.UTF8.GetString(bytes);
+            var utfString = Encoding.GetEncoding(1252).GetString(bytes);
 
             var hits = new List<string>();
-
-            string hexString = null;
-
-            if (withOffsets)
-            {
-                hexString = BitConverter.ToString(bytes);
-            }
 
             foreach (Match match in regUni.Matches(utfString))
             {
                 if (withOffsets)
                 {
-                    var matchBytes = BitConverter.ToString(Encoding.UTF8.GetBytes(match.Value));
+                    //TODO can we do better? http://stackoverflow.com/questions/283456/byte-array-pattern-search/283815#283815
+                    var matchBytes = Encoding.GetEncoding(1252).GetBytes(match.Value);
+                    var pos = ByteSearch(bytes, matchBytes, match.Index);
+                    
+                    var actualOffset = (currentOffset + pos);
 
-
-
-
-                    var foo = hexString.IndexOf(matchBytes, match.Index, StringComparison.Ordinal) / 3;
-
-                    var actualOffset = (currentOffset + foo);
-
-                    hits.Add($"{match.Value.Trim()}{'\0'}{'\0'}0x{actualOffset:X}");
+                    hits.Add($"{match.Value.Trim()}{'\t'}0x{actualOffset:X} (A)");
                 }
                 else
                 {
