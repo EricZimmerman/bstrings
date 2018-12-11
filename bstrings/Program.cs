0b565afab75c041518473380852d88c7208ad987
Eric Zimmerman <saericzimmerman@gmail.com>
2018-12-11 11:59:15 -0500

    nuget, use nlog.config if found

--- bstrings/Program.cs
+++ bstrings/Program.cs
@@ -1015,6 +1015,10 @@ namespace bstrings
 
         private static void SetupNLog()
         {
+            if (File.Exists("Nlog.config"))
+            {
+                return;
+            }
             var config = new LoggingConfiguration();
             var loglevel = LogLevel.Info;
 
