e9853207201d203d4afeec3f499c7345a7fc441a
Eric Zimmerman <saericzimmerman@gmail.com>
2018-12-12 10:16:28 -0500

    nlog path tweak

--- bstrings/Program.cs
+++ bstrings/Program.cs
@@ -1012,10 +1012,10 @@ namespace bstrings
             return hits;
         }
 
-
+        private static readonly string BaseDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
         private static void SetupNLog()
         {
-            if (File.Exists("Nlog.config"))
+            if (File.Exists( Path.Combine(BaseDirectory,"Nlog.config")))
             {
                 return;
             }
