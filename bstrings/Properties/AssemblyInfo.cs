967e81d5ecfef54318ca688dbd7d00fe2f9ed524
Eric Zimmerman <saericzimmerman@gmail.com>
2020-02-06 14:50:52 -0500

    nuget

--- bstrings/Properties/AssemblyInfo.cs
+++ bstrings/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Eric Zimmerman")]
 [assembly: AssemblyProduct("bstrings")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("saericzimmerman@gmail.com")]
 [assembly: AssemblyCulture("")]
 
